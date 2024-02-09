b4cf12fd7e116ea046feca4983d8b417158e6e94
LizMsj <Moysey9g@mail.ru>
2024-02-09 13:19:39 +0300

    first scenes and sprites. easy start game

---
+++ scenes/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -260.0
+
+@onready var animation = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animation.play("fly")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		animation.play("fly")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+		
+	if direction == -1:
+		animation.flip_h = true
+	if direction == 1:
+		animation.flip_h = false
+	move_and_slide()
